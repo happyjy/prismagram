0acf5e9d4e13e1f864579ced35ed1ab29e57b94c
happyjy <okwoyjy@gmail.com>
2019-10-03 17:26:37 +0900

    #1.1 Creating GraphQL Server, #1.2 Setting Up the Server like the Pros

---
+++ src/schema.js
@@ -0,0 +1,17 @@
+import path from "path";
+import { makeExecutableSchema } from "graphql-tools"; 
+import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
+
+
+//api폴더 및에 모든 폴더 및의 모든 graphql, js파일을 받아와 
+//아래 makeExecuteableSchema function을 통해서 schema를 만들어 합친다.
+//server.js에서 imoprt해서 사용하면 되겠다.
+const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
+const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
+
+const schema = makeExecutableSchema({
+  typeDefs: mergeTypes(allTypes),
+  resolvers: mergeResolvers(allResolvers)
+})
+
+export default schema;
--- src/server.js
+++ src/server.js
@@ -1,2 +1,34 @@
-console.log('hello');
-console.log('server!!!');
+require("dotenv").config()
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema.js";
+
+const PORT = process.env.PORT || 4000;
+
+/* 
+//schema.js파일을 만들어 아래는 주석처리
+// const typeDefs = `
+//   type Query{
+//       hello: String!
+//   }
+// `;
+
+// const resolvers = {
+//   Query: {
+//     hello: () => "Hi"
+//   }
+// };
+// const server = new GraphQLServer({ typeDefs, resolvers });
+ */
+
+//서버생성
+const server = new GraphQLServer({schema});
+
+//morgan (logger middleware 추가)
+//GraphQLServer에는 express server가 내장되어 있음.
+server.express.use(logger("dev"));
+
+//start function 추가
+server.start({ port: PORT }, () => 
+  console.log(`✅ Server running on http://localhost:${PORT}`)
+);
