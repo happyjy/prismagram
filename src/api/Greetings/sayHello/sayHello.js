8b341e8a1b5e80582e4128993281d060ca129ed4
happyjy <okwoyjy@gmail.com>
2019-10-04 09:50:27 +0900

    #2.3 Integrating Prisma in our Server

--- src/api/Greetings/sayHello/sayHello.js
+++ src/api/Greetings/sayHello/sayHello.js
@@ -1,5 +1,11 @@
+import { prisma } from "../../../../generated/prisma-client";
+
 export default {
   Query: {
-    sayHello: () => "Hello"
+    sayHello: async () => {
+      // console.log("hello")
+      console.log(await prisma.user());
+      return "HELLO"
+    }
   }
 };
