b07bcc4ed36fa107980eb67bac793d9dbbcba7c1
happyjy <okwoyjy@gmail.com>
2019-10-08 22:50:11 +0900

    init User.lginSecret feild after Login

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,6 +7,13 @@ export default {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret === secret) {
+        //로그인후 User.loginSecret 초기화
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: {
+            loginSecret: ""
+          }
+        });
         //JWT
         return generateToken(user.id);
       } else {
@@ -14,4 +21,4 @@ export default {
       }
     }
   }
-}
+};
