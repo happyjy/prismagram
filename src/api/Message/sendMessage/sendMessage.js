59dbc35cdba49529c43c5ceef41c65b61b07a31d
happyjy <okwoyjy@gmail.com>
2019-10-23 22:49:06 +0900

    #3.24 sendMessage part Two

--- src/api/Message/sendMessage/sendMessage.js
+++ src/api/Message/sendMessage/sendMessage.js
@@ -1,4 +1,5 @@
 import { prisma } from "../../../../generated/prisma-client";
+import { ROOM_FRAGMENT } from "../../../fragment";
 
 export default {
   Mutation: {
@@ -7,28 +8,46 @@ export default {
       const { user } = request;
       const { roomId, message, toId } = args;
       let room;
-      //  console.log(roomId);
-      if (roomdId === undefined) {
+      //  console.log("###: ", roomId);
+      if (roomId === undefined) {
         //스스로에게 message를 보내지 않기 위한 용도
-        if (user.id !== to) {
-          const room = await prisma.createRoom({
+        if (user.id !== toId) {
+          room = await prisma.createRoom({
             participants: {
-              connect: [{ id: to }, { id: user.id }]
+              connect: [{ id: toId }, { id: user.id }]
             }
-          });
+          }).$fragment(ROOM_FRAGMENT);
         }
       } else {
-        room = await prisma.room({ id: roomId });
+        room = await prisma.room({ id: roomId }).$fragment(ROOM_FRAGMENT);
       }
+
+      console.log(room)
+      const getTo = room.participants.filter(
+        participant => participant.id !== user.id
+      )[0];
 
       if(!room){
         throw Error("Room not found");
       }
 
-      // [수정 필요]아래 to의 대상이 toId뿐만 아니라 participant 되어야 한다.
-      // const message = await prisma.createMessage({text: message, to:toId})
-      return true;
-
+      return prisma.createMessage({
+        text: message,
+        from: {
+          connect: { id: user.id }
+        },
+        to: {
+          connect: {
+            id: roomId ? getTo.id : toId
+          }
+        },
+        room: {
+          connect: {
+            id: room.id
+          }
+        }
+      });
+      
        //room을 찾고 message를 거기에 넣는다. 
        //만약 room이 없다면 participants구성원으로 rooms을 만들고 message를 보낸다.
     }
