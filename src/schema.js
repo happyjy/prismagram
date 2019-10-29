220266e8f6aa392a44eb14ced37450231848078c
happyjy <okwoyjy@gmail.com>
2019-10-29 23:43:03 +0900

    오타수정

--- src/schema.js
+++ src/schema.js
@@ -4,7 +4,7 @@ import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 
 
 //api폴더 및에 모든 폴더 및의 모든 graphql, js파일을 받아와 
-//아래 makeExecuteableSchema function을 통해서 schema를 만들어 합친다.
+//아래 makeExecutableSchema function을 통해서 schema를 만들어 합친다.
 //server.js에서 imoprt해서 사용하면 되겠다.
 const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
 const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
