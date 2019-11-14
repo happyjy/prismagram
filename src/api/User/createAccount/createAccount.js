aa3caaebc11ab1a26242d1bda80edf7377500691
happyjy <okwoyjy@gmail.com>
2019-11-15 00:10:57 +0900

    #5.6 step_ change return type of createAccount Mutation

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -6,14 +6,14 @@ export default {
       // console.log("### prisma: ", prisma);
       // promise 객체라는 것을 확인할 수 있다.: [Symbol(Symbol.toStringTag)]: 'Promise' },
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
+      await prisma.createUser({
         username,
         email,
         firstName,
         lastName,
         bio
       });
-      return user;
+      return true;
     }
   }
 }
